3d6a6e64d7b74d80ae606a56a6a7a7cc05a2d1d2
Ayla <dogan-ayla@hotmail.com>
2025-11-07 16:16:13 +0100

    done

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-window.addEventListener('scroll', () => {
-    const header = document.querySelector('section');
-    section.forEach(section => {
-        const rect = section.getBoundingClientRect();
-        if(rect.top < window.innerHeight -100) {
-            section.classList.add('visible');
-        }
-    })
+window.addEventListener("DOMContentLoaded", () => {
+  const sidebar = document.querySelector(".sidebar");
+  const main = document.querySelector("main");
+  const intro = document.querySelector("#intro");
+
+  // On écoute le défilement de la page
+  window.addEventListener("scroll", () => {
+    const introRect = intro.getBoundingClientRect();
+
+    // Quand le bas de la section intro quitte le haut de l'écran
+    if (introRect.bottom <= 60) {
+      sidebar.classList.add("left");
+      main.classList.add("shifted");
+    } else {
+      sidebar.classList.remove("left");
+      main.classList.remove("shifted");
+    }
+  });
 });
